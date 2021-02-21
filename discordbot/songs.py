485a0b2d70aa7f4b6995e28c07120d2fdbd0f226
billiem <billiemerz@gmail.com>
2021-02-21 20:13:18 +0000

    song class complete, added methods to dir/ queue, began refactoring core for changes

--- discordbot/songs.py
+++ discordbot/songs.py
@@ -1,26 +1,16 @@
 import os
 import shutil
-from youtubesearchpython import VideosSearch
+import random
 
-'''
-may also move the dir classes to a seperate file
-maybe dir object is defined with the queue???
-i think dirs object is created on run, & then passed to each queue instance
-'''
+from youtubesearchpython import VideosSearch
+from pytube import YouTube
+from pydub import AudioSegment
+from pysndfx import AudioEffectsChain
 
 class Song():
 
     '''
     Class used for downloading songs/ processing audio
-
-    required methods
-        get song url from search term
-        extract information from videosearch object
-        check if song already downloaded
-        download song from youtube
-        convert mp4 to wav
-        convert wav to mp4
-        add audioeffects
 
     also note, right now stream audio files are stacking up
         these should be deleted on stream finish
@@ -29,9 +19,10 @@ class Song():
     
     ideally want to use multithreading for this whole process (definitely for download)
     '''
-    def __init__(self, dirs, searchTerm, speed = 1, reverb = 0, overdrive = 0):
+    def __init__(self, dirs, searchTerm, reverse = False, speed = 1, reverb = 0, overdrive = 0):
         self.dirs = dirs
         self.searchTerm = searchTerm
+        self.reverse = reverse
         self.speed = speed
         self.reverb = reverb
         self.overdrive = overdrive
@@ -39,6 +30,7 @@ class Song():
         self.title = None
         self.duration = None
         self.url = None
+        self.fileName = None
 
     def __repr__(self):
         '''
@@ -52,6 +44,11 @@ class Song():
     def processSong(self):
         self.getVideoSearchObject()
         self.extractInfoFromVideoSearch()
+        self.downloadFromYoutube()
+        self.mp4ToWav()
+        self.addAudioEffects()
+        self.wavToMp4()
+        self.preStreamClean()
 
     def getVideoSearchObject(self):
         '''
@@ -69,14 +66,70 @@ class Song():
         self.title = self.videoSearch['title']
         self.duration = self.videoSearch['duration']  
         self.url = self.videoSearch['link']
+        self.fileName = self.videoSearch['id']
+
+    def downloadFromYoutube(self):
+        downloadedAudioDirPath = self.dirs['downloadedAudio'].dirPath
+        songPath = f'{downloadedAudioDirPath}/{self.fileName}.mp4'
+
+        if not os.path.isfile(songPath):
+            yt = YouTube(self.url)
+            audioStream = yt.streams.filter(only_audio=True).first()
+            audioStream.download(downloadedAudioDirPath, filename=self.fileName)
+
+    def mp4ToWav(self):
+        mp4DirPath = self.dirs['downloadedAudio'].dirPath
+        wavDirPath = self.dirs['wavAudio'].dirPath
+        mp4Path = f'{mp4DirPath}/{self.fileName}.mp4'
+        wavPath = f'{wavDirPath}/{self.fileName}.wav'
+        audio = AudioSegment.from_file(mp4Path)
+        audio.export(wavPath, format = 'wav')
+
+    def wavToMp4(self):
+        wavDirPath = self.dirs['processedAudio'].dirPath
+        mp4DirPath = self.dirs['streamAudio'].dirPath
+        wavPath = f'{wavDirPath}/{self.fileName}.wav'
+        mp4Path = f'{mp4DirPath}/{self.fileName}.mp4'
+        audio = AudioSegment.from_file(wavPath)
+        audio.export(mp4Path, format = 'mp4')
+
+    def addAudioEffects(self):
+        fx = AudioEffectsChain()
+
+        if self.reverse:
+            fx.reverse()
+
+        if self.speed != None:
+            fx.speed(factor = self.speed)
+
+        if self.reverb != None:
+            fx.reverb(reverberance = self.reverb)
+        
+        if self.overdrive != None :
+            fx.overdrive(gain = self.overdrive)
+
+        wavDirPath = self.dirs['wavAudio'].dirPath
+        processedDirPath = self.dirs['processedAudio'].dirPath
+        wavPath = f'{wavDirPath}/{self.fileName}.wav'
+        processedPath = f'{processedDirPath}/{self.fileName}.wav'
+
+        fx(wavPath, processedPath)
+
+    def preStreamClean(self):
+        self.dirs.preStreamClean(self.fileName)
+
+    def postStreamClean(self):
+        self.dirs.postStreamClean(self.fileName)
 
 if __name__ == '__main__':
 
     from dirs import Dirs
 
     dirs = Dirs()
+    dirs.dirsSetup()
 
     searchTerm = input('What song would you like to search for? ')
+    reverse = False
     speed = input('How much would you like to speed up the song? (1 = normal speed, 2 = double speed) ')
     reverb = input('How much reverb would you like to add? (0-100) ')
     overdrive = input('How much overdrive would you like to add? ')
@@ -84,6 +137,7 @@ if __name__ == '__main__':
     song = Song(
         dirs = dirs,
         searchTerm = searchTerm,
+        reverse = reverse,
         speed = speed,
         reverb = reverb,
         overdrive = overdrive
